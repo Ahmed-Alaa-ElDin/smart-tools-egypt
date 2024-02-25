9ca2e32e6dec07a6f48227a3549c8ab19ad216e3
Ahmed-Alaa-ElDin-Ishowcase <ahmed@ishowcaseinc.com>
2024-02-25 16:53:50 +0200

    FIX:: top banner

--- resources/views/livewire/front/homepage/top-banner.blade.php
+++ resources/views/livewire/front/homepage/top-banner.blade.php
@@ -1,14 +1,14 @@
-@if ($banner && $banner->banner_name != 'Top Banner')
-    <div class="relative bg-white border-b w-full overflow-hidden h-[50px] text-center">
-        <a href="{{ $banner->link }}">
-            <img class="w-full" src="{{ asset('storage/images/banners/original/' . $banner->banner_name) }}"
-                alt="{{ $banner->banner_name }}">
-        </a>
-        <span
-            class="material-icons remove_banner_button text-sm font-bold flex items-center justify-center absolute rtl:left-3 ltr:right-3 top-[50%] -translate-y-[50%] cursor-pointer text-black rounded-circle w-6 h-6 bg-white">
-            close
-        </span>
-    </div>
-@else
-    <div></div>
-@endif
+<div>
+    @if ($banner && $banner->banner_name != 'Top Banner')
+        <div class="relative bg-white border-b w-full overflow-hidden h-[50px] text-center">
+            <a href="{{ $banner->link }}">
+                <img class="w-full" src="{{ asset('storage/images/banners/original/' . $banner->banner_name) }}"
+                    alt="{{ $banner->banner_name }}">
+            </a>
+            <span
+                class="material-icons remove_banner_button text-sm font-bold flex items-center justify-center absolute rtl:left-3 ltr:right-3 top-[50%] -translate-y-[50%] cursor-pointer text-black rounded-circle w-6 h-6 bg-white">
+                close
+            </span>
+        </div>        
+    @endif
+</div>
