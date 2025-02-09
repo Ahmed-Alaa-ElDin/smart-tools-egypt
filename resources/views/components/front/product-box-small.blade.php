a4bb2150e2a77ce27c0a064f1d1c3b3aa4691335
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-02-09 20:09:45 +0200

    FEAT :: Edit the image sizes and valid offers

--- resources/views/components/front/product-box-small.blade.php
+++ resources/views/components/front/product-box-small.blade.php
@@ -46,6 +46,8 @@
                 </span>
                 {{-- Base Discount : End --}}
 
+                @dump($item['thumbnail'])
+                
                 {{-- Product Image : Start --}}
                 <div class="block max-h-52 overflow-hidden">
                     @if ($item['thumbnail'])
