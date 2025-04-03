277fba5083e108f35eb01b8ae17556aea45ddd8f
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-03 14:02:54 +0200

    IMPROVE :: Improve homepage loading by resizing the banners

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,4 +24,10 @@ mix.js("resources/js/app.js", "public/assets/js")
 
 if (mix.inProduction()) {
     mix.version();
+    // mix.minify("public/assets/css/app.css");
+    // mix.minify("public/assets/css/material-dashboard.min.css");
+    // mix.minify("public/assets/css/material-dashboard-rtl.css");
+    // mix.minify("public/assets/css/select2.min.css");
+    // mix.minify("public/assets/css/splide.min.css");
+    // mix.minify("public/assets/js/app.js");
 }
