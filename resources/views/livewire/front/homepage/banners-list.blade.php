28bc142755a16b1de0ec26fb2596f1d73892003b
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-03 13:26:32 +0200

    IMPROVE :: Improve homepage loading by resizing the banners

--- resources/views/livewire/front/homepage/banners-list.blade.php
+++ resources/views/livewire/front/homepage/banners-list.blade.php
@@ -6,7 +6,7 @@
                     <div class="group relative col-span-6 lg:col-span-4  overflow-hidden ">
                         <a href="{{ env('APP_URL') . session('locale') . '/' . $banner->link }}"
                             class="block shadow rounded overflow-hidden after:content-[''] after:bg-gray-100 after:opacity-10 after:absolute after:-rotate-45 after:w-full after:h-full after:top-0 after:right-full after:transition after:ease-in-out after:delay-150 group-hover:after:left-s group-hover:after:translate-x-full">
-                            <img src="{{ asset('storage/images/banners/original/' . $banner->banner_name) }}"
+                            <img src="{{ asset('storage/images/banners/cropped500/' . $banner->banner_name) }}"
                                 alt="{{ $banner->banner_name }}" class="img-fluid w-[500px] lazyloaded rounded object-cover object-center">
                         </a>
                     </div>
@@ -14,7 +14,7 @@
                     <div class="group relative col-span-6 col-start-4 lg:col-span-4 lg:col-start-0  overflow-hidden ">
                         <a href="{{ env('APP_URL') . session('locale') . '/' . $banner->link }}"
                             class="block shadow rounded overflow-hidden after:content-[''] after:bg-gray-100 after:opacity-10 after:absolute after:-rotate-45 after:w-full after:h-full after:top-0 after:right-full after:transition after:ease-in-out after:delay-150 group-hover:after:left-s group-hover:after:translate-x-full">
-                            <img src="{{ asset('storage/images/banners/original/' . $banner->banner_name) }}"
+                            <img src="{{ asset('storage/images/banners/cropped500/' . $banner->banner_name) }}"
                                 alt="{{ $banner->banner_name }}" class="img-fluid w-[500px] lazyloaded rounded object-cover object-center">
                         </a>
                     </div>
