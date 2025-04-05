b967fde09b26191b391d4dd1b48c479b3c97db33
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-05 08:16:44 +0200

    FIX :: Fix the livewire update error 403

--- resources/views/livewire/front/homepage/header-search-box.blade.php
+++ resources/views/livewire/front/homepage/header-search-box.blade.php
@@ -36,7 +36,82 @@
                     {{-- Loading :: End --}}
 
                     {{-- Products List :: Start --}}
-                    
+                    @forelse ($items as $item)
+                        <a @if ($item['type'] == 'Product') href="{{ route('front.products.show', ['id' => $item['id'], 'slug' => $item['slug'][session('locale')]]) }}"
+                            @elseif ($item['type'] == 'Collection') href="{{ route('front.collections.show', ['id' => $item['id'], 'slug' => $item['slug'][session('locale')]]) }}" @endif
+                            class="group flex justify-center items-center gap-4 cursor-pointer rounded transition-all ease-in-out hover:bg-red-100 hover:text-current p-2"
+                            wire:key="item-{{ $item['id'] }}-{{ $item['type'] }}"
+                            data-iteration="{{ $loop->index }}">
+
+                            {{-- Product Image  --}}
+                            @if ($item['thumbnail'])
+                                <div
+                                    class="flex justify-center items-center min-w-[100px] min-h-[100px] w-[100px] h-[100px] overflow-hidden">
+                                    <img @if ($item['type'] == 'Product') src="{{ asset('storage/images/products/original/' . $item['thumbnail']['file_name']) }}"
+                                        @elseif ($item['type'] == 'Collection') src="{{ asset('storage/images/collections/original/' . $item['thumbnail']['file_name']) }}" @endif
+                                        alt="{{ $item['thumbnail']['file_name'] . '-image' }}"
+                                        class="min-w-100 min-h-100">
+                                </div>
+                            @else
+                                <div class="flex justify-center items-center bg-gray-100 w-[100px] h-[100px]">
+                                    <span class="block material-icons text-[100px]">
+                                        construction
+                                    </span>
+                                </div>
+                            @endif
+
+                            <div class="w-100 overflow-hidden flex flex-col gap-2">
+                                {{-- Product's Name --}}
+                                <div class="flex flex-col justify-start ltr:text-left rtl:text-right gap-2 grow">
+                                    <span class="font-bold text-black">{{ $item['name'][session('locale')] }}</span>
+                                    @if (isset($item['brand']))
+                                        <span
+                                            class="text-sm font-bold text-gray-500">{{ $item['brand'] ? $item['brand']['name'] : '' }}</span>
+                                    @endif
+                                </div>
+
+                                {{-- Product's Model --}}
+                                @if ($item['model'])
+                                    <div class="inline-block w-75 truncate text-xs">
+                                        <span class="text-gray-500">{{ __('front/homePage.Model') }}:</span>
+                                        <span class="text-gray-800">
+                                            {{ $item['model'] }}
+                                        </span>
+                                    </div>
+                                @endif
+
+                                {{-- Product's Description --}}
+                                @if ($item['description'])
+                                    <div class="inline-block w-75 truncate text-xs max-h-4">
+                                        <span class="text-gray-500 font-bold">
+                                            @if (session('locale') == 'en' && isset($item['description']['en']))
+                                                {!! $item['description']['en'] !!}
+                                            @elseif (session('locale') == 'ar' && isset($item['description']['ar']))
+                                                {!! $item['description']['ar'] !!}
+                                            @endif
+                                        </span>
+                                    </div>
+                                @endif
+
+                            </div>
+
+                        </a>
+
+                        @if (!$loop->last)
+                            <hr class="my-1">
+                        @else
+                            <div class="flex justify-center w-full mt-2 ">
+                                <a href="#" wire:click="seeMore"
+                                    class="inline-block w-full font-bold text-white text-center rounded-xl bg-secondary py-2 ">
+                                    {{ __('front/homePage.See More & Apply filter') }}
+                                </a>
+                            </div>
+                        @endif
+                    @empty
+                        <div class="text-center font-bold">
+                            {{ __('admin/offersPages.No Products or Collections Found') }}
+                        </div>
+                    @endforelse
                     {{-- Products List :: End --}}
                 </div>
                 {{-- Search Results :: End --}}
