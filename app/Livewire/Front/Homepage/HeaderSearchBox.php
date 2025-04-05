da22979dc75905ad84cb24f12204f73ece31a054
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-05 13:02:46 +0200

    FIX :: Fix the livewire update error 403

--- app/Livewire/Front/Homepage/HeaderSearchBox.php
+++ app/Livewire/Front/Homepage/HeaderSearchBox.php
@@ -17,103 +17,82 @@ class HeaderSearchBox extends Component
 
     public function updatedSearch($key)
     {
-        $this->items = Product::select([
-            'id',
-            'name',
-            'slug',
-            'barcode',
-            'original_price',
-            'base_price',
-            'final_price',
-            'under_reviewing',
-            'points',
-            'description',
-            'model',
-            'brand_id'
-        ])
-            ->with(
-                'brand',
-                'thumbnail'
-            )
-            ->where('publish', 1)
-            ->take(10)
-            ->get()
-            ->toArray();
-        // if ($this->search) {
-        //     $products = Product::select([
-        //         'id',
-        //         'name',
-        //         'slug',
-        //         'barcode',
-        //         'original_price',
-        //         'base_price',
-        //         'final_price',
-        //         'under_reviewing',
-        //         'points',
-        //         'description',
-        //         'model',
-        //         'brand_id'
-        //     ])
-        //         ->with(
-        //             'brand',
-        //             'thumbnail'
-        //         )
-        //         ->where(
-        //             fn ($q) =>
-        //             $q->whereRaw(
-        //                 "MATCH(name,description) AGAINST(?)",
-        //                 array(trim($this->search))
-        //             )->orWhere('barcode', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('name', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('original_price', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('base_price', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('final_price', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('description', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('model', 'like', '%' . $this->search . '%')
-        //                 ->orWhereHas('brand', fn ($q) => $q->where('brands.name', 'like', '%' . $this->search . '%'))
-        //         )
-        //         ->where('publish', 1)
-        //         ->take(value: 10)
-        //         ->get();
+        $this->items = [];
+        if ($this->search) {
+            $products = Product::select([
+                'id',
+                'name',
+                'slug',
+                'barcode',
+                'original_price',
+                'base_price',
+                'final_price',
+                'under_reviewing',
+                'points',
+                'description',
+                'model',
+                'brand_id'
+            ])
+                ->with(
+                    'brand',
+                    'thumbnail'
+                )
+                ->where(
+                    fn ($q) =>
+                    $q->whereRaw(
+                        "MATCH(name,description) AGAINST(?)",
+                        array(trim($this->search))
+                    )->orWhere('barcode', 'like', '%' . $this->search . '%')
+                        ->orWhere('name', 'like', '%' . $this->search . '%')
+                        ->orWhere('original_price', 'like', '%' . $this->search . '%')
+                        ->orWhere('base_price', 'like', '%' . $this->search . '%')
+                        ->orWhere('final_price', 'like', '%' . $this->search . '%')
+                        ->orWhere('description', 'like', '%' . $this->search . '%')
+                        ->orWhere('model', 'like', '%' . $this->search . '%')
+                        ->orWhereHas('brand', fn ($q) => $q->where('brands.name', 'like', '%' . $this->search . '%'))
+                )
+                ->where('publish', 1)
+                ->take(value: 10)
+                ->get();
 
-        //     $collections = Collection::select([
-        //         'id',
-        //         'name',
-        //         'slug',
-        //         'barcode',
-        //         'original_price',
-        //         'base_price',
-        //         'final_price',
-        //         'under_reviewing',
-        //         'points',
-        //         'description',
-        //         'model'
-        //     ])
-        //         ->with('thumbnail')
-        //         ->where(
-        //             fn ($q) =>
-        //             $q->whereRaw(
-        //                 "MATCH(name,description) AGAINST(?)",
-        //                 array(trim($this->search))
-        //             )->orWhere('barcode', 'like', '%' . $this->search . '%')
-        //             ->orWhere('name', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('original_price', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('base_price', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('final_price', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('description', 'like', '%' . $this->search . '%')
-        //                 ->orWhere('model', 'like', '%' . $this->search . '%')
-        //         )
-        //         ->where('publish', 1)
-        //         ->take(10)
-        //         ->get();
+            $collections = Collection::select([
+                'id',
+                'name',
+                'slug',
+                'barcode',
+                'original_price',
+                'base_price',
+                'final_price',
+                'under_reviewing',
+                'points',
+                'description',
+                'model'
+            ])
+                ->with('thumbnail')
+                ->where(
+                    fn ($q) =>
+                    $q->whereRaw(
+                        "MATCH(name,description) AGAINST(?)",
+                        array(trim($this->search))
+                    )->orWhere('barcode', 'like', '%' . $this->search . '%')
+                    ->orWhere('name', 'like', '%' . $this->search . '%')
+                        ->orWhere('original_price', 'like', '%' . $this->search . '%')
+                        ->orWhere('base_price', 'like', '%' . $this->search . '%')
+                        ->orWhere('final_price', 'like', '%' . $this->search . '%')
+                        ->orWhere('description', 'like', '%' . $this->search . '%')
+                        ->orWhere('model', 'like', '%' . $this->search . '%')
+                )
+                ->where('publish', 1)
+                ->take(10)
+                ->get();
 
-        //     $this->items = $collections->concat($products)->map(function ($product_collection) {
-        //         $product_collection->product_collection = class_basename($product_collection);
-        //         return $product_collection;
-        //     })->toArray();
-        // } else {
-        //     $this->items = collect([]);
-        // }
+            $this->items = $collections->concat($products)->map(function ($product_collection) {
+                $product_collection->product_collection = class_basename($product_collection);
+                return $product_collection;
+            });
+        } else {
+            $this->items = collect([]);
+        }
     }
 
     public function seeMore()
