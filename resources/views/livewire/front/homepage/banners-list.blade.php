ebdcf6594c97c173bc7ba75aed29dcf2a47e3920
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-09 15:05:37 +0200

    IMPROVE :: Improve the banners

--- resources/views/livewire/front/homepage/banners-list.blade.php
+++ resources/views/livewire/front/homepage/banners-list.blade.php
@@ -3,7 +3,7 @@
         <div class="grid grid-cols-12 gap-3 items-center justify-between">
             @foreach ($banners as $banner)
                 @if (!$loop->last)
-                    <div class="group relative col-span-6 lg:col-span-4  overflow-hidden ">
+                    <div class="group relative col-span-12 md:col-span-6 lg:col-span-4  overflow-hidden ">
                         <a href="{{ str_starts_with($banner->link, 'http') ? $banner->link : env('APP_URL') . $banner->link }}"
                             class="block shadow rounded overflow-hidden after:content-[''] after:bg-gray-100 after:opacity-10 after:absolute after:-rotate-45 after:w-full after:h-full after:top-0 after:right-full after:transition after:ease-in-out after:delay-150 group-hover:after:left-s group-hover:after:translate-x-full">
                             <img src="{{ asset('storage/images/banners/cropped500/' . $banner->banner_name) }}"
@@ -11,7 +11,7 @@
                         </a>
                     </div>
                 @else
-                    <div class="group relative col-span-6 col-start-4 lg:col-span-4 lg:col-start-0  overflow-hidden ">
+                    <div class="group relative col-span-12 md:col-span-6 md:col-start-4 lg:col-span-4 lg:col-start-0  overflow-hidden ">
                         <a href="{{ str_starts_with($banner->link, 'http') ? $banner->link : env('APP_URL') . $banner->link }}"
                             class="block shadow rounded overflow-hidden after:content-[''] after:bg-gray-100 after:opacity-10 after:absolute after:-rotate-45 after:w-full after:h-full after:top-0 after:right-full after:transition after:ease-in-out after:delay-150 group-hover:after:left-s group-hover:after:translate-x-full">
                             <img src="{{ asset('storage/images/banners/cropped500/' . $banner->banner_name) }}"
