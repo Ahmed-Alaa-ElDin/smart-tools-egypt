8a21b0da27724168c08a72871c410c2d46f1158e
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-13 05:51:39 +0200

    EDIT :: Add banner hover effect

--- resources/views/livewire/front/homepage/main-slider.blade.php
+++ resources/views/livewire/front/homepage/main-slider.blade.php
@@ -88,8 +88,8 @@
         {{-- SubSlider Banners : Start --}}
         <div class="grid grid-cols-1 md:grid-cols-2 gap-3 justify-between items-center overflow-hidden">
             @foreach ($subsliderBanners as $subsliderBanner)
-                <div class="overflow-hidden text-center">
-                    <a
+                <div class="group relative overflow-hidden text-center">
+                    <a class="after:content-[''] after:bg-gray-100 after:opacity-10 after:absolute after:-rotate-45 after:w-full after:h-full after:top-0 after:right-full after:transition after:ease-in-out after:delay-150 group-hover:after:left-s group-hover:after:translate-x-full"
                         href="{{ str_starts_with($subsliderBanner->banner->link, 'http') ? $subsliderBanner->banner->link : env('APP_URL') . $subsliderBanner->banner->link }}">
                         <img loading="lazy"
                             src="{{ asset('storage/images/banners/cropped500/' . $subsliderBanner->banner->banner_name) }}"
@@ -104,8 +104,8 @@
         <div class="flex flex-wrap gap-2 justify-between items-center overflow-hidden">
             @foreach ($subsliderSmallBanners as $subsliderSmallBanner)
                 <div
-                    class="w-[100px] h-[100px] lg:w-[150px] lg:h-[150px] flex justify-center items-center shadow rounded overflow-hidden bg-white text-center">
-                    <a
+                    class="group relative w-[100px] h-[100px] lg:w-[150px] lg:h-[150px] flex justify-center items-center shadow rounded overflow-hidden bg-white text-center">
+                    <a class="after:content-[''] after:bg-gray-100 after:opacity-10 after:absolute after:-rotate-45 after:w-full after:h-full after:top-0 after:right-full after:transition after:ease-in-out after:delay-150 group-hover:after:left-s group-hover:after:translate-x-full"
                         href="{{ str_starts_with($subsliderSmallBanner->banner->link, 'http') ? $subsliderSmallBanner->banner->link : env('APP_URL') . $subsliderSmallBanner->banner->link }}">
                         <img loading="lazy"
                             src="{{ asset('storage/images/banners/cropped150/' . $subsliderSmallBanner->banner->banner_name) }}"
