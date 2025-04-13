b4d23680f62e54b298d1b2c43f972d75ddec35c7
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-13 14:16:39 +0200

    FEAT :: Edit the add to cart UI

--- resources/views/livewire/front/general/back-to-stock-notification/back-to-stock-notification-button.blade.php
+++ resources/views/livewire/front/general/back-to-stock-notification/back-to-stock-notification-button.blade.php
@@ -1,7 +1,7 @@
 <button
     @if ($isNotified) disabled @else wire:click.stop="addToNotify({{ $item_id }},'{{ $type }}')" @endif
     title="@if ($isNotified) {{ __('front/homePage.Will be notified') }} @else {{ __('front/homePage.Notify me when available') }} @endif"
-    class="stop-propagation rounded-full h-9 text-center shadow inline-flex justify-center items-center gap-2 min-w-max
+    class="stop-propagation rounded-full h-9 text-center shadow inline-flex justify-center items-center gap-2 min-w-max mb-3
     @if ($isNotified) disabled text-primary bg-white border border-primary @else text-white bg-primary hover:bg-secondary animate-pulse @endif
     @if ($large) p-6 w-full @endif
     @if ($text) transition ease-in-out p-3 hover:animate-none @else w-9 @endif">
--- resources/views/livewire/front/general/cart/add-to-cart-button.blade.php
+++ resources/views/livewire/front/general/cart/add-to-cart-button.blade.php
@@ -1,7 +1,7 @@
 <button wire:click.stop="addToCart({{ $item_id }},'{{ $type }}')"
     title="@if ($add_buy == 'add') {{ __('front/homePage.Add to cart') }} @elseif($add_buy == 'pay') __('front/homePage.Go to payment') @elseif ($add_buy == 'buy'){{ __('front/homePage.Buy Again') }} @endif"
     class="stop-propagation rounded-full h-9 text-center shadow text-white
-    @if ($inCart && !$large) hidden @else inline-flex justify-center items-center gap-2 min-w-max  mb-3 @endif
+    @if ($inCart && !$large) hidden @else inline-flex justify-center items-center gap-2 min-w-max mb-3 @endif
     @if ($add_buy == 'pay') bg-secondary hover:bg-secondaryDark @else animate-pulse bg-primary hover:bg-secondary @endif
     @if ($large) p-6 w-full @endif
     @if ($text) transition ease-in-out p-3 hover:animate-none hover:bg-secondary hover:text-white
