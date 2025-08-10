1d78cdca7aac08e199c29780d12b5a46da457931
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-08-10 19:45:39 +0300

    WORKING :: Working on integration of Meta Pixel

---
+++ app/Services/Front/meta/MetaPixelService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class MetaPixelService
+{
+    protected $pixelId;
+    protected $accessToken;
+    protected $apiVersion;
+
+    public function __construct()
+    {
+        $this->pixelId = config('services.meta_pixel.id');
+        $this->accessToken = config('services.meta_pixel.access_token');
+        $this->apiVersion = config('services.meta_pixel.api_version');
+    }
+
+    public function sendEvent(string $eventName, array $userData, array $customData = [])
+    {
+        $endpoint = "https://graph.facebook.com/{$this->apiVersion}/{$this->pixelId}/events";
+
+        $payload = [
+            'data' => [
+                [
+                    'event_name' => $eventName,
+                    'event_time' => now()->timestamp,
+                    'action_source' => 'website', // could be 'email', 'phone_call', etc.
+                    'user_data' => $this->hashUserData($userData),
+                    'custom_data' => $customData,
+                ]
+            ],
+            'access_token' => $this->accessToken,
+        ];
+
+        return Http::post($endpoint, $payload)->json();
+    }
+
+    private function hashUserData(array $data)
+    {
+        // Meta requires SHA256 hashes for PII
+        return collect($data)
+            ->mapWithKeys(function ($value, $key) {
+                return [$key => hash('sha256', strtolower(trim($value)))];
+            })
+            ->toArray();
+    }
+}
